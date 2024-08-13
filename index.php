e168cca7bb37649b36d48ea5adbd6b33b2f7ca92
ayyashkun <airayyash7@gmail.com>
2024-08-13 10:45:43 +0700

    adding a html and css files

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+$angka1 = $_GET['num1'];
+$angka2 = $_GET['num2'];
+$hasil = $angka1+$angka2;
+?>
+
+<html>
+    <form action="" method="get">
+        <label for="">Angka 1</label>
+        <input type="number" placeholder="angka 1" name="num1">
+        <br>
+        <label for="">Angka 2</label>
+        <input type="number" placeholder="angka 2" name="num2">
+        <button type="submit">Jumlahkan</button>
+    </form>
+</html>
+
+<h2> <?php echo"Hasil : $hasil"; ?></h2>
